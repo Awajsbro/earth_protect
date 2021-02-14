b0bee573ce8130d99e790425df5849f4f70cfe5a
Awajsbro <awajsbro@student.42.fr>
2021-02-14 18:52:59 +0100

    test request

--- components/GamePage.js
+++ components/GamePage.js
@@ -1,45 +1,58 @@
 import React, { useState, useEffect } from 'react'
 import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native'
-import { asteroidRadius, asteroidSpeed, shootWidth, shootStartY, shootSpeed, asteroidRng, life, chargeBeamWidth, delayBetweenShoot } from "../settings.json"
+import { asteroidRadius, asteroidSpeed, shootWidth, shootStartY, shootSpeed, asteroidRng, life, chargeBeamWidth, delayBetweenShoot, radiusBlast } from "../settings.json"
 import Asteroid from "./Asteroid"
 import Earth from "./Earth"
 import Shoot from "./Shoot"
 import ChargeBeam from "./ChargeBeam"
+import MegaBomb from "./MegaBomb"
 import HealthBar from "./HealthBar"
 import GameOver from "./gameOver"
 
 const GamePage = ({ screenHeight, screenWidth, backHome }) => {
 	const [asteroids, setAsteroids] = useState({})
 	const [shoots, setShoots] = useState({})
-	const [newShoot, setNewShoot] = useState({})
+	const [newShoot, setNewShoot] = useState(null)
 	const [blockShoot, setBlockShoot] = useState(false)
 	const [chargeBeam, setChargeBeam] = useState(false)
+	const [megaBomb, setMegaBomb] = useState(null)
 	const [nbAsteroidsDestroyed, setNbAsteroidsDestroyed] = useState(0)
 	const [nbAsteroidsLand, setNbAsteroidsLand] = useState(0)
 	const [shootsCount, setShootsCount] = useState(0)
-	const [newTap, setNewTap] = useState(null)
+	const [newTap, setNewTap] = useState(false)
+	const [newTapForLong, setNewTapForLong] = useState(false)
 	const [gameStart, setGameStart] = useState(false)
 	const [gameOver, setGameOver] = useState(false)
 	const [theWorld, setTheWorld] = useState(false)
 	let asteroidsTimerId
 	let shootsTimerId
-	let doubleTapTimerId
+	let megaBombTimerId
+
+
+	const [tmp, settmp] = useState([])
 
 
 	// Vector calculator
-	const vectorCalculator = (x, y, targetX, targetY) => {
-		let angle = Math.atan(Math.abs(x - targetX) / Math.abs(y - targetY))
+	const angleCalculator = (x, y, targetX, targetY) => {
+		const angle = Math.atan(Math.abs(x - targetX) / Math.abs(y - targetY))
 
-		if (x < targetX)
-			angle = -angle
-
-		return [Math.sin(angle), Math.cos(angle), angle]
+		return x < targetX ? -angle : angle
 	}
 
 
 	// Distance calculator
 	const distCalculator = (x, y, targetX, targetY) => {
 		return Math.sqrt(Math.pow(x - targetX, 2) + Math.pow(y - targetY, 2))
+	}
+
+
+	// linear interpolation
+	const linearInterpolation = (x, y, targetX, targetY, t) => {
+		const angle = -angleCalculator(x, y, targetX, targetY)
+		const dist = distCalculator(x, y, targetX, targetY)
+		const deltaY = y < targetY ? -dist * Math.cos(angle) * t : dist * Math.cos(angle) * t
+
+		return [x + (dist * Math.sin(angle) * t), y - deltaY]
 	}
 
 
@@ -53,7 +66,7 @@ const GamePage = ({ screenHeight, screenWidth, backHome }) => {
 			setShootsCount(0)
 			setAsteroids({})
 			setShoots({})
-			setNewShoot({})
+			setNewShoot(null)
 			setGameOver(false)
 			console.log("restart")
 		}, 100)
@@ -69,33 +82,23 @@ const GamePage = ({ screenHeight, screenWidth, backHome }) => {
 				setNewTap(false)
 				sendMegaBomb(x, y)
 			} else {
-				setNewTap(true)
 				if (!chargeBeam || !blockShoot)
 					shootMaker(x, y)
+				setNewTapForLong(true)
+				setNewTap(true)
+				setTimeout(() => { setNewTap(false) }, 200)
+				setTimeout(() => { setNewTapForLong(false) }, 550)
 			}
 	}
+
 
 	const longPressHandler = (x, y) => {
-		console.log(newTap)
-		if (newTap && !chargeBeam) {
-			setNewTap(false)
+		if (newTapForLong && !chargeBeam) {
+			setNewTapForLong(false)
 			chargeBeamGenerator(x, y)
-		} else {
-			setNewTap(true)
-			if (!theWorld)
-				stopTime()
-		}
+		} else
+			stopTime()
 	}
-
-	// Double tap reset
-	useEffect(() => {
-		doubleTapTimerId = setInterval(() => {
-			if (newTap) {
-				setNewTap(false)
-			}
-		}, 200)
-		return () => clearInterval(doubleTapTimerId)
-	}, [newTap])
 
 
 	// Za warudo
@@ -106,13 +109,81 @@ const GamePage = ({ screenHeight, screenWidth, backHome }) => {
 
 	// ===============================  Shoots ===============================
 
+	// MegaBomb create
+	const sendMegaBomb = (targetX, targetY) => {
+		const dist = distCalculator(screenWidth / 2, screenHeight - shootStartY, targetX, targetY)
+		const angle = angleCalculator(screenWidth / 2, screenHeight - shootStartY, targetX, targetY)
+		const distMidPt = (dist / 2) / Math.cos(0.3)
+		let angleMidPt
+
+		if (angle > 0.5)
+			angleMidPt = 0.3
+		else if (angle > 0.3)
+			angleMidPt = angle - 0.3
+		else if (angle < -0.5)
+			angleMidPt = -0.3
+		else if (angle < -0.3)
+			angleMidPt = angle + 0.3
+		else
+			angleMidPt = 0
+		setMegaBomb({
+			t: 0,
+			x: screenWidth / 2,
+			y: shootStartY,
+			midPtX: screenWidth / 2 - Math.sin(angleMidPt) * distMidPt,
+			midPtY: screenHeight - shootStartY - Math.cos(angleMidPt) * distMidPt,
+			targetX,
+			targetY,
+			radius: 0,
+			angle: 0,
+		})
+	}
+
+
+	// MegaBomb interact
+	useEffect(() => {
+		megaBombTimerId = setInterval(() => {
+			if (megaBomb)
+				if (theWorld) {
+					setMegaBomb({ ...megaBomb })
+				} else if (megaBomb.t < 1) {
+					const pt1 = linearInterpolation(screenWidth / 2, screenHeight - shootStartY, megaBomb.midPtX, megaBomb.midPtY, megaBomb.t)
+					const pt2 = linearInterpolation(megaBomb.midPtX, megaBomb.midPtY, megaBomb.targetX, megaBomb.targetY, megaBomb.t)
+					const ptMegaBomb = linearInterpolation(pt1[0], pt1[1], pt2[0], pt2[1], megaBomb.t)
+
+					setMegaBomb({
+						...megaBomb,
+						t: megaBomb.t + 0.01,
+						x: ptMegaBomb[0],
+						y: ptMegaBomb[1],
+						angle: pt1[1] > pt2[1] ? angleCalculator(pt1[0], pt1[1], pt2[0], pt2[1]) : -angleCalculator(pt1[0], pt1[1], pt2[0], pt2[1]),
+					})
+				} else if (megaBomb.radius < radiusBlast && !megaBomb.maxSize) {
+					setMegaBomb({
+						...megaBomb,
+						radius: megaBomb.radius + 5,
+						maxSize: megaBomb.radius + 5 < radiusBlast ? megaBomb.maxSize : true
+					})
+				} else if (megaBomb.radius > 20) {
+					setMegaBomb({
+						...megaBomb,
+						radius: megaBomb.radius - 20
+					})
+				} else {
+					setMegaBomb(null)
+				}
+		}, 30)
+		return () => { clearInterval(megaBombTimerId) }
+	}, [megaBomb])
+
+
 	// Charge beam generator
 	const chargeBeamGenerator = (targetX, targetY) => {
-		const [vectorX, vectorY, angle] = vectorCalculator(screenWidth / 2, screenHeight - shootStartY, targetX, targetY)
+		const angle = angleCalculator(screenWidth / 2, screenHeight - shootStartY, targetX, targetY)
 
 		setChargeBeam({
-			x: (screenWidth - screenHeight * vectorX) / 2,
-			y: - shootStartY + screenHeight / 2 - vectorY * screenHeight / 2,
+			x: (screenWidth - screenHeight * Math.sin(angle)) / 2,
+			y: - shootStartY + screenHeight / 2 - Math.cos(angle) * screenHeight / 2,
 			angle,
 		})
 		setTimeout(() => {
@@ -120,10 +191,11 @@ const GamePage = ({ screenHeight, screenWidth, backHome }) => {
 		}, delayBetweenShoot);
 	}
 
+
 	// Shoots generator
 	const shootMaker = (targetX, targetY) => {
 		const id = Math.random()
-		const [vectorX, vectorY, angle] = vectorCalculator(screenWidth / 2, screenHeight - shootStartY, targetX, targetY)
+		const angle = angleCalculator(screenWidth / 2, screenHeight - shootStartY, targetX, targetY)
 
 		setBlockShoot(true)
 		setNewShoot({
@@ -132,8 +204,8 @@ const GamePage = ({ screenHeight, screenWidth, backHome }) => {
 			y: screenHeight - shootStartY,
 			deltaHitbox1: [-2 * Math.sin(angle), 2 * Math.cos(angle)],
 			deltaHitbox2: [2 * Math.sin(angle), -2 * Math.cos(angle)],
-			vectorX,
-			vectorY,
+			vectorX: Math.sin(angle),
+			vectorY: Math.cos(angle),
 			angle,
 		})
 		setTimeout(() => {
@@ -149,9 +221,9 @@ const GamePage = ({ screenHeight, screenWidth, backHome }) => {
 			let shootsTmp = { ...shoots }
 
 			// Add new shoot
-			if (newShoot.id != undefined && !Object.keys(shootsTmp).includes(newShoot.id)) {
+			if (newShoot && !Object.keys(shootsTmp).includes(newShoot.id)) {
 				shootsTmp[newShoot.id] = newShoot
-				setNewShoot({})
+				setNewShoot(null)
 				setShootsCount(shootsCount + 1)
 			}
 
@@ -196,13 +268,26 @@ const GamePage = ({ screenHeight, screenWidth, backHome }) => {
 			let idS = Object.keys(shoots).map(id => id)
 			let asteroidsTmp = { ...asteroids }
 
-			// Asteroids land
 			Object.keys(asteroidsTmp).forEach(idA => {
+
+				// Asteroids land
 				if (asteroidsTmp[idA].y > screenHeight) {
 					setNbAsteroidsLand(nbAsteroidsLand => nbAsteroidsLand + 1)
 					delete asteroidsTmp[idA]
 					if (nbAsteroidsLand >= life - 1)
 						setGameOver(true)
+
+					// Destruction by chargeBeam
+				} else if (chargeBeam) {
+					const dist = distCalculator(screenWidth / 2, screenHeight - shootStartY, asteroidsTmp[idA].x, asteroidsTmp[idA].y)
+					const angle = angleCalculator(screenWidth / 2, screenHeight - shootStartY, asteroidsTmp[idA].x, asteroidsTmp[idA].y)
+					if (dist * Math.abs(Math.sin(angle - chargeBeam.angle)) < asteroidRadius - chargeBeamWidth / 3)
+						delete asteroidsTmp[idA]
+
+					// Destruction by megaBomb
+				} else if (megaBomb && megaBomb.radius > 0) {
+					if (megaBomb.radius > distCalculator(megaBomb.x, megaBomb.y, asteroidsTmp[idA].x, asteroidsTmp[idA].y))
+						delete asteroidsTmp[idA]
 				}
 			})
 
@@ -219,15 +304,6 @@ const GamePage = ({ screenHeight, screenWidth, backHome }) => {
 				})
 			}
 
-			// Destruction by chargeBeam
-			if (chargeBeam) {
-				Object.keys(asteroidsTmp).forEach(idA => {
-					const dist = distCalculator(screenWidth / 2, screenHeight - shootStartY, asteroidsTmp[idA].x, asteroidsTmp[idA].y)
-					const [, , angle] = vectorCalculator(screenWidth / 2, screenHeight - shootStartY, asteroidsTmp[idA].x, asteroidsTmp[idA].y)
-					if (dist * Math.abs(Math.sin(angle - chargeBeam.angle)) < asteroidRadius - chargeBeamWidth / 3)
-						delete asteroidsTmp[idA]
-				})
-			}
 
 			if (!theWorld) {
 				// Asteroids move
@@ -243,15 +319,15 @@ const GamePage = ({ screenHeight, screenWidth, backHome }) => {
 				if (gameStart && Math.random() > asteroidRng) {
 					const id = Math.random()
 					const x = Math.random() * (screenWidth - asteroidRadius * 2) + asteroidRadius
-					const [vectorX, vectorY] = vectorCalculator(x, 0, Math.random() * screenWidth, screenHeight)
+					const angle = angleCalculator(x, 0, Math.random() * screenWidth, screenHeight)
 					const ownSpeed = asteroidSpeed * (Math.random() + 0.5)
 
 					asteroidsTmp[id] = {
 						id,
 						x,
 						y: -asteroidRadius,
-						horizontalSpeed: vectorX * ownSpeed,
-						verticalSpeed: vectorY * ownSpeed,
+						horizontalSpeed: Math.sin(angle) * ownSpeed,
+						verticalSpeed: Math.cos(angle) * ownSpeed,
 					}
 				}
 			}
@@ -276,6 +352,17 @@ const GamePage = ({ screenHeight, screenWidth, backHome }) => {
 				/>
 			})}
 		</>
+	}
+
+
+	// MegaBomb render
+	const megaBombRender = () => {
+		return <MegaBomb
+			x={megaBomb.x}
+			y={megaBomb.y}
+			angle={megaBomb.angle}
+			radius={megaBomb.radius}
+		/>
 	}
 
 
@@ -311,12 +398,13 @@ const GamePage = ({ screenHeight, screenWidth, backHome }) => {
 		>
 			{gameStart ? null :
 				<Text style={{ color: "white" }}>
-					{`Tap\t\t\tshoot\nHold\t\tcharge beam\nDouble tap\tざわ-るど`}
+					{`Tap\t\t\t\tshoot\nHold\t\t\tざわ-るど\nDouble tap\t\tmega bomb\nHold double tap\tcharge beam`}
 				</Text>
 			}
 			<Earth screenHeigth={screenHeight} />
 			{asteroidsRender()}
 			{shootsRender()}
+			{megaBomb ? megaBombRender() : null}
 			{chargeBeam ? <ChargeBeam
 				x={chargeBeam.x}
 				y={chargeBeam.y}
@@ -332,6 +420,9 @@ const GamePage = ({ screenHeight, screenWidth, backHome }) => {
 				backHome={backHome}
 			/>
 				: null}
+			{/* {tmp.map(t => {
+				// return <View style={{ backgroundColor: "green", height: 10, width: 10, position: "absolute", top: t[1], left: t[0] }} />
+			})} */}
 			{/* <View style={{ position: "absolute", bottom: shootStartY, height: 200, width: 20, left: screenWidth / 2, backgroundColor: "red", transform: [{ rotate: `${0}rad` }], }} /> */}
 		</TouchableOpacity>
 	)
---
+++ components/MegaBomb.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { shootHeight, shootWidth } from "../settings.json"
+
+const MegaBomb = ({ x, y, angle, radius }) => {
+
+	if (radius === 0)
+		return <View style={{
+			position: "absolute",
+			backgroundColor: "red",
+			height: shootHeight,
+			width: shootWidth,
+			transform: [{ rotate: `${-angle}rad` }],
+			top: y,
+			left: x,
+		}} />
+	else
+		return <View style={{
+			position: "absolute",
+			backgroundColor: "yellow",
+			height: radius * 2,
+			width: radius * 2,
+			borderRadius: radius,
+			top: y - radius,
+			left: x - radius,
+		}} />
+}
+
+export default MegaBomb

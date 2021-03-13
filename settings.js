5a585a8b076f5d595c8e12d1b6b69bd2fa8e8810
Kevin <34973659+keketiger@users.noreply.github.com>
2021-03-13 19:05:55 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,23 +22,40 @@ export const lang = "fr"
 export const language = {
 	en: {
 		LANG: "English",
+
 		BUTTON_PLAY: "Play",
 		BUTTON_UPGRADES: "Upgrades",
 		BUTTON_SETTINGS: "Settings",
 		BUTTON_SCORE: "Score",
+
 		GAME_OVER_HITS: "Hits",
-		GAME_OVER_BONUS: "Precision bonus"
+		GAME_OVER_BONUS: "Precision bonus",
+
+		SCORE_TOTAL_PLAYED: "Number of games played",
+		SCORE_TOTAL_HITS: "Total number of hits",
+		SCORE_AVERAGE: "Average score",
+		SCORE_BEST: "Best score",
+		SCORE_PRECISION: "Average precision",
+		SCORE_BEST_PRECISION: "Best precision"
 
 	},
 	fr: {
 		LANG: "Français",
+
 		BUTTON_PLAY: "Jouer",
 		BUTTON_UPGRADES: "Amélioration",
 		BUTTON_SETTINGS: "Options",
 		BUTTON_SCORE: "Score",
+
 		GAME_OVER_HITS: "Coups",
-		GAME_OVER_BONUS: "Bonus de précision"
+		GAME_OVER_BONUS: "Bonus de précision",
+
+		SCORE_TOTAL_PLAYED: "Nombre de parties joué",
+		SCORE_TOTAL_HITS: "Nombre total de tirs",
+		SCORE_AVERAGE: "Score moyen",
+		SCORE_BEST: "Meilleur score",
+		SCORE_PRECISION: "Précision moyenne",
+		SCORE_BEST_PRECISION: "Meilleure précision"
 
 	}
 }
-
